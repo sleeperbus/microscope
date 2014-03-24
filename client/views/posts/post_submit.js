2fa126f34a4a7080ca908b14a3442d776267f752
Hojoon Ji <sleeperbus@gmail.com>
2014-03-24 23:36:14 +0900

    Error package created

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -9,8 +9,15 @@ Template.postSubmit.events({
 		}
 
 		Meteor.call('post', post, function(error, id) {
-			if (error) return alert(error.reason);
+			if (error) {
+				// display the error to the user
+				Errors.throw(error.reason);
+				if (error.error == 302)
+					Router.go('postPage', {_id: error.details});
+				else {
+					Router.go('postPage', {_id: id});
+				}
+			}
 		});
-		Router.go('postsList');
 	}
 });
